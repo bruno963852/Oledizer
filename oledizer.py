d10c1cb6aa64dae6e3011d6fc844ac3983626c9f
Bruno Martins <bruno963852@gmail.com>
2019-11-20 10:27:04 -0300

    added docs and format

--- oledizer.py
+++ oledizer.py
@@ -8,12 +8,18 @@ parser.add_argument('--output', '-o', metavar='-o', type=str, nargs='?',
                    help='name of the output file')
 parser.add_argument('--crop', '-c', action='store_true', default=False,
                     help='if the image should be cropped')
-
+parser.add_argument('--format', '-f', default="bytes", nargs='?', choices=['bytes', 'bool'],
+                    help='The format of the output')
 
 args = parser.parse_args()
 print(args)
 
-def image_to_boolean_array(image):
+def image_to_boolean_tuple(image: Image):
+    """ Gets the Image object and converts it to a matrix of booleans
+    :param image: The image to be converted
+
+    :rtype: tuple
+    """
     pixels = image.load()
 
     width, height = image.size
@@ -29,7 +35,44 @@ def image_to_boolean_array(image):
     
     return output
 
-def generate_icon_file(image_file, output_file=None, crop=False):
+def image_to_bytes(image):
+    """ Gets the Image object and converts it to tuple with widht, height and bytes with the image data
+    :param image: The image to be converted
+
+    :rtype: tuple
+    """
+    pixels = image.load()
+
+    width, height = image.size
+
+    imagebytes = bytearray()
+    output = [width, height]
+
+    bytecounter = 0
+    mbyte = 0
+    for y in range(height):
+        for x in range(width):
+            _, _, _, alpha = pixels[x, y]
+            if (alpha > 0):
+                mbyte = mbyte | (1 << bytecounter)
+            bytecounter = bytecounter + 1
+            if bytecounter >= 8:
+                imagebytes.append(mbyte)
+                bytecounter = 0
+                mbyte=0
+    output.append(bytes(imagebytes))
+    return tuple(output)
+
+def generate_icon_file(image_file: str, output_file: str = None, crop: bool = False, output_format: str = 'bytes'):
+    """ Gets an image file and converts it to a python file to be used to draw the image on
+        :param image_file: the filename of the image
+    
+        :param output_file: filename of the output file
+    
+        :param crop: if the image should be cropped
+    
+        :param output_format: the format of the output file, bytes or bool
+    """
     try:
         image = Image.open(args.image, mode='r')
     except IOError:
@@ -39,7 +82,7 @@ def generate_icon_file(image_file, output_file=None, crop=False):
     if crop:
         image = image.crop(image.getbbox())
 
-    output = image_to_boolean_array(image)
+    output = image_to_bytes(image) if output_format == 'bytes' else image_to_boolean_tuple(image)
 
     if output_file is None:
         print(output)
